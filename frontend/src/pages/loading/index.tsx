04c101790827ec19e490fc9414711fcbc6ec37e0
Jasson Acea <jacea@dwmedios.com>
2024-08-05 14:02:26 -0600

    Splash screen, loading, react router

---
+++ frontend/src/pages/loading/index.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="flex flex-col min-h-screen  bg-green-600">
+      <div className="flex-grow flex justify-center items-center">
+        <img src="/images/loading.png" alt="Logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
