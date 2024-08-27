9118720ebd403f69d85c7583615be28b8fcd3468
Ricardo May Jiménez <57809006+r88amj@users.noreply.github.com>
2024-08-27 17:04:51 -0600

    Merge branch 'feat/splashscreen-loading' of https://github.com/DWMedios/aeco-app into feat/home-view-pages

--- frontend/src/views/finalView/index.tsx
+++ frontend/src/views/finalView/index.tsx
@@ -9,7 +9,7 @@ const FinalView = () => {
         <h1 className='text-5xl font-normal z-10 mb-12 w-[400px]'>¿TE INTERESA EL PROYECTO?</h1>
         <img src="/public/images/QRcode.png" alt="QR Code" className='h-[420px] z-10' />
         <button className='border-[#027333] border-solid border-4 p-4 rounded-lg text-[#027333] text-3xl font-bold w-[350px] z-10 cursor-pointer'>ayuntaeco.com</button>
-        <p className='text-2xl font-normal z-10 tracking-wider w-[200px]'>¡Gracias por tu colaboracion!</p>
+        <p className='text-2xl font-normal z-10 tracking-wider w-[200px]'>¡Gracias por tu colaboración!</p>
       </div>
     </ScreenLayout>
   );
