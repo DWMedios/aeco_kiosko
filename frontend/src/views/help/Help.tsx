2a4725b8ccd626a9fcc8ea79acbffd3b9abf3b06
franciscocamposchimal <camposfranciscoing@gmail.com>
2024-09-19 16:59:44 -0600

    feat(front):add api connection home view

--- frontend/src/views/help/Help.tsx
+++ frontend/src/views/help/Help.tsx
@@ -1,18 +1,24 @@
 import BackButton from "../../components/backButton/BackButton";
 import ScreenLayout from "../../components/layout/screenLayout";
 import QRCodeComponent from '../../components/qrCode';
+import { usePageData } from '../../hooks/usePageData';
+import { MetaDataHelp } from '../../interfaces';
 
 function Help() {
   const QrCodeUrl = 'https://wa.me/9861190181?text=Hola%20Ayuntaeco%20|%20¡Necesito%20ayuda!';
 
+  const { data: metas} = usePageData<MetaDataHelp>('Help');
+
+  if (!metas) return <div>No metadata available</div>;
+
   return (
-    <ScreenLayout image="help_background.svg">
+    <ScreenLayout image={metas.background}>
       <div className="text-center h-screen flex flex-col justify-center items-center select-none gap-11">
       <BackButton url="/home" imageSrc='images/backbutton.png'/>
-        <h1 className="text-8xl mt-24  mb-10 max-w-[300px]">Soporte</h1>
+        <h1 className="text-8xl mt-24  mb-10 max-w-[300px]">{metas.textCenter.title}</h1>
         <p className="text-4xl max-w-[500px] mb-5">
-          Cualquier duda o aclaración estamos para escucharte. <br />
-          <br /> Envianos un WhatsApp{" "}
+          {metas.textCenter.description} <br />
+          <br /> {metas.textCenter.phoneText}
         </p>
        <div className='w-full z-10 flex justify-center'>
         <QRCodeComponent value={QrCodeUrl}/>
@@ -23,9 +29,9 @@ function Help() {
           alt="QR Code"
         />
         <p className="text-5xl font-bold max-w-[450px] mb-5 mt-8">
-          +52 999 888 7777{" "}
+          {metas.textDown.phone}
         </p>
-        <p className="text-4xl max-w-[400px]">¡Gracias por tu colaboración! </p>
+        <p className="text-4xl max-w-[400px]">{metas.textDown.description}</p>
       </div>
     </ScreenLayout>
   );
