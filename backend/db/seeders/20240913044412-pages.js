1a104bbec4d9853c30b4ff97dbf9f773ef69518b
franciscocamposchimal <camposfranciscoing@gmail.com>
2024-09-19 16:51:23 -0600

    feat(backend): categories, pages seeder

--- backend/db/seeders/20240913044412-pages.js
+++ backend/db/seeders/20240913044412-pages.js
@@ -1,10 +1,128 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const pages = [
+      {
+        id: 1,
+        name: 'Home',
+        metadata: {
+          background:'home_background.png',
+          logoLang:{
+            path:'/images/language_icon.svg',
+            alt:'Language icon'
+          },
+          logoHelp:{
+            path:'/images/Help_icon.svg',
+            alt:'Help icon'
+          },
+          logoUp:{
+            path:'/images/aeco.svg',
+            alt:'AECO Logo up'
+          },
+          logoDown:{
+            path:'/images/reciclaygana.svg',
+            alt:'Recicla y Gana Logo down'
+          },
+          button:{
+            label:'INICIAR',
+            bgColor:'pink',
+            url:'/conditions',
+            borderRadious:'full',
+            fontSize:'xl8'
+          }
+        }
+      },
+      {
+        id: 2,
+        name: 'Language',
+        metadata: {
+          background: 'language_background.svg',
+          button: {
+            background:'language_background.svg',
+            button:{
+              labelEs:'ACEPTAR',
+              labelEn:'AGREE',
+              bgColor:'white',
+              url:'/home',
+              borderRadious:'xl3',
+              fontSize:'xl6'
+            }
+          }
+        },
+      },
+      {
+        id: 3,
+        name: 'Help',
+        metadata: {
+          background: 'help_background.svg',
+          textCenter: {
+            title: 'Soporte',
+            description: 'Cualquier duda o aclaración estamos para escucharte.',
+            phoneText: 'Envíanos WhatsApp',
+          },
+          textDown: {
+            phone: '+52 999 888 7777',
+            description: '¡Gracias por tu colaboración!',
+          },
+        },
+      },
+      {
+        id: 4,
+        name: 'Conditions',
+        metadata: {
+          background: 'shrubbery.png',
+          title: 'Recompensas Disponibles',
+          description: 'Tus envases deben estar en las siguientes condiciones para poder ser reciclados:',
+          button: {
+            label: '¡Entendido!',
+            bgColor: 'white',
+            url: '/example',
+            borderRadious: 'xl3',
+            fontSize: 'xl6',
+          },
+          lists: [
+            {
+              title: 'Envases de PET',
+              icon: '/images/bottle.png',
+              items: [
+                'Sin aplastar.',
+                'Con etiqueta.',
+                'Sin residuos.',
+              ],
+            },
+            {
+              title: 'Latas de Aluminio',
+              icon: '/images/can.png',
+              items: [
+                'Sin aplastar.',
+                'Código de barras visible.',
+                'Sin residuos.',
+              ],
+            },
+          ],
+        },
+      },
+      {
+        id: 5,
+        name: 'Example',
+        metadata: {
+          background: 'leafBackground.png',
+          description: 'Inserta tu envase con el código de barras hacia arriba',
+          centerImage: '/images/example.png',
+          button: {
+            label: '¡Estoy listo!',
+            bgColor: 'white',
+            url: '/insert',
+            borderRadious: 'xl3',
+            fontSize: 'xl6',
+          },
+        },
+      }
+    ]
     const query = `INSERT INTO "pages" (id, name, metadata) VALUES 
-                      (1,'Home','{"background":"home_background.png","logoLang":{"path":"/images/language_icon.svg","alt":"Language icon"},"logoHelp":{"path":"/images/Help_icon.svg","alt":"Help icon"},"logoUp":{"path":"/images/aeco.svg","alt":"AECO Logo up"},"logoDown":{"path":"/images/reciclaygana.svg","alt":"Recicla y Gana Logo down"},"button":{"label":"INICIAR","bgColor":"pink","url":"/conditions","borderRadious":"full","fontSize":"xl8"}}'),
-                      (2,'Language', '{"background":"language_background.svg","button":{"labelEs":"ACEPTAR","labelEn":"AGREE","bgColor":"white","url":"/home","borderRadious":"xl3","fontSize":"xl6"}}'),
-                      (3,'Help', '{"background":"help_background.svg","textCenter":{"title":"Soporte","description":"Cualquier duda o aclaración estamos para escucharte.","phoneText":"Envíanos WhatsApp"},"textDown":{"phone":"+52 999 888 7777","description":"¡Gracias por tu colaboración!"}}');`
+                      ${pages.map(page => {
+    return `(${page.id},'${page.name}','${JSON.stringify(page.metadata)}')`
+  }).join(',')};`
 
     await queryInterface.sequelize.query(query, {
       type: Sequelize.QueryTypes.INSERT,
