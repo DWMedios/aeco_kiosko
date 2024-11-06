d5b5dbc67f381134453ef81f64ae7a173855004b
Jasson Acea <jacea@dwmedios.com>
2024-11-06 11:29:12 -0600

    rename column into model

--- backend/db/models/Company.js
+++ backend/db/models/Company.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING(150),
         allowNull: true,
       },
-      mac_address: {
+      macAddress: {
         type: DataTypes.STRING(17),
         allowNull: false,
         unique: true,
