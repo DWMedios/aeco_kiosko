561484657cd704b19b4a4ecaa08d2fcaf970a618
jassonx <aceaxiu@gmail.com>
2025-02-20 14:49:23 -0600

    models

--- backend/repositories/movementRepository.js
+++ backend/repositories/movementRepository.js
@@ -16,7 +16,7 @@ exports.create = async (data) => {
 exports.findOne = async (id) => {
   const { Movement } = await initializeDatabase()
   return await Movement.findOne({
-    where: { id },
+    where: { id, printed_ticket: 0 },
     attributes: { exclude: ['createdAt', 'updatedAt'] },
   })
 }
