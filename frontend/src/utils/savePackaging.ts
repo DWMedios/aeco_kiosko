0cfdd495391995128d524e1e7b5ed5012ee3a7c5
jassonx <aceaxiu@gmail.com>
2025-05-14 10:45:14 -0600

    format code

--- frontend/src/utils/savePackaging.ts
+++ frontend/src/utils/savePackaging.ts
@@ -5,25 +5,25 @@ import { getSessionStorage, setSessionStorage } from './manageStorage'
 export const SavePackaging = async (packaging: Packaging) => {
   try {
     const ticket = GetTicket() || defaultPackaging
-  const existing = ticket.packagings.find((p) => p.id === packaging.id)
+    const existing = ticket.packagings.find((p) => p.id === packaging.id)
 
-  if (existing) {
-    existing.quantity = (existing.quantity || 1) + 1
-  } else {
-    ticket.packagings.push({ ...packaging, quantity: 1 })
-  }
-  const updatedProducts = {
-    packagings: [...ticket.packagings],
-    total_cans: ticket.total_cans + (packaging.packagingType === 'can' ? 1 : 0),
-    total_bottles:
-      ticket.total_bottles + (packaging.packagingType === 'bottle' ? 1 : 0),
-  }
-  setSessionStorage('ticket', JSON.stringify(updatedProducts))
-  return true
+    if (existing) {
+      existing.quantity = (existing.quantity || 1) + 1
+    } else {
+      ticket.packagings.push({ ...packaging, quantity: 1 })
+    }
+    const updatedProducts = {
+      packagings: [...ticket.packagings],
+      total_cans:
+        ticket.total_cans + (packaging.packagingType === 'can' ? 1 : 0),
+      total_bottles:
+        ticket.total_bottles + (packaging.packagingType === 'bottle' ? 1 : 0),
+    }
+    setSessionStorage('ticket', JSON.stringify(updatedProducts))
+    return true
   } catch (error) {
     return false
   }
-  
 }
 
 export const GetTicket = (): Packagings | null => {
