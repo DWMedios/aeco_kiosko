c4a268fea5ed9bfbecde109a439db592db0ee2c6
jassonx <aceaxiu@gmail.com>
2025-08-01 11:47:23 -0600

    Bug fix

--- frontend/src/views/offline/index.tsx
+++ frontend/src/views/offline/index.tsx
@@ -6,7 +6,7 @@ import WebApiAeco from '../../api/webApiAeco'
 
 const Offline = ({ icon = 'FueraServicio' }: Icon) => {
   const navigation = useNavigate()
-  const intervalMs = 360000 // 1 hour
+  const intervalMs = 60000 // 1 minute
   const timerRef = useRef<NodeJS.Timeout>()
 
   useEffect(() => {
